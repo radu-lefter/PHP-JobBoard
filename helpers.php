d8002314c78aced50f484f9bb6d2ba1ade8c88c2
Radu Lefter <lefter_radu@yahoo.com>
2024-02-11 09:44:04 +0000

    created helper functions to inspect values

--- helpers.php
+++ helpers.php
@@ -1,6 +1,33 @@
 <?php
 
 /**
+ * Inspect a value(s)
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspect($value)
+{
+  echo '<pre>';
+  var_dump($value);
+  echo '</pre>';
+}
+
+/**
+ * Inspect a value(s) and die
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspectAndDie($value)
+{
+  echo '<pre>';
+  var_dump($value);
+  echo '</pre>';
+  die();
+}
+
+/**
  * Get the base path
  * 
  * @param string $path
