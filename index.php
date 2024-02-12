19f694db35ee99ab90bdcb6279081bc1b22f1455
Radu Lefter <lefter_radu@yahoo.com>
2024-02-12 17:14:50 +0000

    implemented autoloader and namespaces

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
 require './helpers.php';
+use Framework\Router;
 
-
-spl_autoload_register(function ($class) {
-    $path = basePath('Framework/' . $class . '.php');
-    if (file_exists($path)) {
-      require $path;
-    }
-  });
+// spl_autoload_register(function ($class) {
+//     $path = basePath('Framework/' . $class . '.php');
+//     if (file_exists($path)) {
+//       require $path;
+//     }
+//   });
 
 // Instatiate the router
 $router = new Router();
