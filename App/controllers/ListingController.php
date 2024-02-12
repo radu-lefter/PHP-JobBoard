56cc681f74f618a70202030ff7734c87a9dfbd11
Radu Lefter <lefter_radu@yahoo.com>
2024-02-12 20:18:10 +0000

    refactored handling errors to error controller

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -6,40 +6,54 @@ use Framework\Database;
 
 class ListingController
 {
-  protected $db;
+    protected $db;
 
-  public function __construct()
-  {
-    $config = require basePath('config/db.php');
-    $this->db = new Database($config);
-  }
+    public function __construct()
+    {
+        $config = require basePath('config/db.php');
+        $this->db = new Database($config);
+    }
 
-  public function index()
-  {
-    $listings = $this->db->query('SELECT * FROM listings')->fetchAll();
+    /*
+     * Show all listings
+     * 
+     * @return void
+     */
+    public function index()
+    {
+        $listings = $this->db->query('SELECT * FROM listings')->fetchAll();
 
-    loadView('home', [
-      'listings' => $listings
-    ]);
-  }
+        loadView('home', [
+            'listings' => $listings
+        ]);
+    }
+    /*
+     * Show the create listing form
+     * 
+     * @return void
+     */
+    public function create()
+    {
+        loadView('listings/create');
+    }
 
-  public function create()
-  {
-    loadView('listings/create');
-  }
+    /*
+     * Show a single listing
+     * 
+     * @return void
+     */
+    public function show()
+    {
+        $id = $_GET['id'] ?? '';
 
-  public function show()
-  {
-    $id = $_GET['id'] ?? '';
+        $params = [
+            'id' => $id
+        ];
 
-    $params = [
-      'id' => $id
-    ];
+        $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
 
-    $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
-
-    loadView('listings/show', [
-      'listing' => $listing
-    ]);
-  }
+        loadView('listings/show', [
+            'listing' => $listing
+        ]);
+    }
 }
