0b4d37d993eb431af051d5a3b9a5ed8f8ebbe349
Radu Lefter <lefter_radu@yahoo.com>
2024-02-13 18:12:38 +0000

    implemented flash messages

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <?php
+session_start();
 require __DIR__ . '/vendor/autoload.php';
 require './helpers.php';
 use Framework\Router;
-
-// spl_autoload_register(function ($class) {
-//     $path = basePath('Framework/' . $class . '.php');
-//     if (file_exists($path)) {
-//       require $path;
-//     }
-//   });
 
 // Instatiate the router
 $router = new Router();
@@ -18,8 +12,7 @@ $routes = require basePath('routes.php');
 
 // Get current URI and HTTP method
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$method = $_SERVER['REQUEST_METHOD'];
 
 // Route the request
-$router->route($uri, $method);
+$router->route($uri);
 
