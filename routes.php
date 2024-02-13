9e58c0cbbe7241617a5ce3ebecef0dec6d391d8e
Radu Lefter <lefter_radu@yahoo.com>
2024-02-13 18:42:40 +0000

    implemented functionality to update listing

--- routes.php
+++ routes.php
@@ -7,4 +7,5 @@ $router->get('/listings/edit/{id}', 'ListingController@edit');
 $router->get('/listings/{id}', 'ListingController@show');
 
 $router->post('/listings', 'ListingController@store');
+$router->put('/listings/{id}', 'ListingController@update');
 $router->delete('/listings/{id}', 'ListingController@destroy');
