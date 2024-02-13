241a642c7c30d9809875a82a8186602c27b07888
Radu Lefter <lefter_radu@yahoo.com>
2024-02-13 18:28:34 +0000

    implemented functionality to show the edit form view

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -65,6 +65,33 @@ class ListingController
     }
 
     /**
+   * Show the listing edit form
+   * 
+   * @param array $params
+   * @return void
+   */
+  public function edit($params)
+  {
+    $id = $params['id'] ?? '';
+
+    $params = [
+      'id' => $id
+    ];
+
+    $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+    // Check if listing exists
+    if (!$listing) {
+      ErrorController::notFound('Listing not found');
+      return;
+    }
+
+    loadView('listings/edit', [
+      'listing' => $listing
+    ]);
+  }
+
+    /**
    * Store data in database
    * 
    * @return void
